301f1fa0b607f95b7396ad6f0d8010492a95958a
xxc <xxc4015@163.com>
2019-06-02 15:29:44 +0800

    update_linux_init

--- Env_init/Linux/initLinux.sh
+++ Env_init/Linux/initLinux.sh
@@ -1,49 +1,94 @@
-#!/bin/sh
-echo "开始配置linux"
-echo "检测更新"
-sudo apt update 
-echo "检测完成"
-sudo apt upgrade
-echo "更新完成"
-sudo apt install gdebi
-sudo apt install vim 
-sudo apt install sqlite3
-sudo apt install adb
-sudo apt install tig
-sudo apt install gitk
-sudo apt install meld
-sudo apt install shadowsocks
-sudo apt install wget 
-sudo apt install git 
-sudo apt-get install fonts-wqy-microhei
-sudo apt-get install shutter
-sudo apt install ubuntu-restricted-extras
-sudo apt install p7zip-full
-sudo apt install zip
-sudo apt install vlc
-sudo apt install gitk
-sudo apt install meld
-sudo apt install wps-office
+#!/bin/sh 
 
-#以下是选择安装
-#安装zsh
-#sudo apt-get install zsh
-#安装oh-my-zsh
-#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-#在.zshrc中修改自己喜欢的主题
+echo "###################################"
+echo "#                                #"
+echo "#Auther:   xxc                   #"
+echo "#Version:  1.0                   #"
+echo "#Date:     2019.6.2              #"
+echo "#Des:      Linux环境搭建         #"
+echo "#                                #"
+echo "##################################"
 
-##安装albert
-#sudo add-apt-repository ppa:noobslab/macbuntu
-#sudo apt-get update
-#sudo apt-get install albert
+
+uninstall_unuse_soft(){
+
+	printf -- "\033[37m uninstall unuse soft \033[0m\n"
+        sudo apt-get remove unity-webapps-common
+        sudo apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot
+        sudo apt-get remove gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
+        sudo apt-get remove onboard deja-dup
+        sudo apt-get remove libreoffice-common
+	printf -- "\033[37m install done!  \033[0m\n"
+
+}
+
+installl_usefull_soft(){
+
+	printf -- "\033[32m install usefull soft!  \033[0m\n"
+	sudo apt install gdebi vim wget curl tree shutter zip p7zip-full shadowsocks shutter
+	printf -- "\033[32m install done           \033[0m\n"
+}
 
 
 
+install_android_dev(){
+	
+	printf -- "\033[33m install android dev tools \033[0m\n"
+	sudo apt install adb sqlite3 gcc gdb sqlitebrowser  android-tools-fastboot
+	printf -- "\033[33m install done!             \033[0m\n"
+}
 
-echo "卸载多余的软件"
-sudo apt-get remove unity-webapps-common
-sudo apt-get remove thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot
-sudo apt-get remove gnome-mines cheese transmission-common gnome-orca webbrowser-app gnome-sudoku  landscape-client-ui-install
-sudo apt-get remove onboard deja-dup
-sudo apt-get remove libreoffice-common
 
+install_git_soft(){
+	
+	printf --  "\033[31m install git tools! \033[0m\n"
+	sudo apt install git tig gitk meld 
+	printf --  "\033[31m install done!      \033[0m\n"
+
+}
+
+
+install_fonts(){
+
+	printf -- "\033[33m install font \033[0m\n"
+	sudo apt install fonts-wqy-microhei
+	printf -- "\033[33m install font done \033[0m\n"
+
+}
+
+install_media(){
+
+	printf -- "\033[35m install media \033[0m\n"
+	sudo apt install ubuntu-restricted-extras vlc
+	printf -- "\033[35m install done! \033[0m\n"
+
+}
+
+
+install_oh_my_zsh(){
+
+	printf -- "\033[36m install oh my zsh \033[0m\n"
+	sudo apt install zsh 
+	wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+	printf -- "\033[36m install done!     \033[0m\n"
+	
+}
+
+update_linux(){
+	
+	printf -- "\033[31m  update linux  \033[0m\n"
+	sudo apt update
+	sudo apt upgrade
+	printf -- "\033[31m  update done!  \033[0m\n"
+	
+}
+
+
+
+    update_linux
+    uninstall_unuse_soft
+    installl_usefull_soft
+    install_git_soft
+    install_media
+    install_fonts
+    install_android_dev
